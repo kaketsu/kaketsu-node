09001190995f3ce869cfc246a902324a68cf6476
kaketsu <huajiewuxi@gmail.com>
2016-03-22 17:39:00 +0800

    add starter module

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+/*
 var express = require('express');
 var favicon = require('serve-favicon');
 var fs = require('fs');
 var logger = require('morgan');
 var app = express();
+
+//favicon.ico
 app.use(favicon(__dirname + '/favicon.ico'));
 
+//log
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
 app.use(logger('combined', {stream: accessLogStream}));
 
@@ -13,4 +17,21 @@ app.get('/', function(req, res){
 });
 app.listen(1024);
 console.log('Server running at http://127.0.0.1:1024/');
+*/
 
+//***********************
+//使用node中的http模块
+/*var http = require("http");
+
+http.createServer(function(request, response){
+	console.log("Request received");
+	response.writeHead(200, {"Content-Type":"text/plain"});
+	response.write("Hello qiuqiu");
+	response.end();
+}).listen(1025);*/
+
+
+//使用模块进行封装createrServer
+var server = require('./start');
+server.start();
+console.log("Server has started.");
